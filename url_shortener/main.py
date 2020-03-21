cb065e66fc0a9f27b3b5757831deb86f8ad2ecb1
Vlad <kuklavudu78@gmail.com>
2020-03-21 13:09:17 +0200

    Added alpha inline query

--- url_shortener/main.py
+++ url_shortener/main.py
@@ -31,5 +31,24 @@ async def handle_link(msg: types.Message):
     await msg.reply(f'<b>Invalid link</b>')
     logger.error(f'Error message of response: {response["error"]["error_msg"]}. Error code: {response["error"]["error_code"]}')
 
+
+@dp.inline_handler(regexp=r'^(https?:\/\/[^\s]+)$')
+async def handle_inline_link(inline_query: types.InlineQuery):
+    text = inline_query.query
+    response = (await client.VKClient.get('/method/utils.getShortLink', params={'url': text})).json()
+
+    if response.get('response'):
+        input_content = types.InputTextMessageContent(response['response']['short_url'])
+    else:
+        input_content = types.InputTextMessageContent('Invalid link')
+    result_id = hashlib.md5(text.encode()).hexdigest()
+    item = types.InlineQueryResultArticle(
+        id=result_id,
+        title=f'Short link: {response["response"]["short_url"]}',
+        input_message_content=input_content
+    )
+
+    await bot.answer_inline_query(inline_query.id, results=[item], cache_time=1)
+
 if __name__ == '__main__':
     executor.start_polling(dp)
