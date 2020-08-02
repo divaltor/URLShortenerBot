f6644143c548707b592f419ed735494ce40232d3
Vlad <kuklavudu78@gmail.com>
2020-08-02 19:53:08 +0300

    Fixed bug with redis

--- app/config.py
+++ app/config.py
@@ -11,4 +11,4 @@ BOT_TOKEN = env.str('BOT_TOKEN')
 
 REDIS_HOST = env.str('REDIS_HOST', default='localhost')
 REDIS_PORT = env.int('REDIS_PORT', default=6379)
-REDIS_PASS = env.str('REDIS_PASS')
+REDIS_PASS = env.str('REDIS_PASS', default=None)
